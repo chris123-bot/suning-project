2b01e0a7ac814232fc82ce020577f9cfed8c3059
shadowtao718 <tao1576415299@163.com>
2020-11-18 16:52:39 +0800

    网站修改提交

--- src/snRouter.vue
+++ src/snRouter.vue
@@ -6,11 +6,11 @@ import mine from "./components/home/mine.vue";
 import rankinglist from "./components/home/ranklist.vue";
 import roblist from "./components/home/roblisting.vue";
 import shopcart from "./components/home/shopcart.vue";
-import prodetail from "./components/home/prodetail.vue";
+import prodetail from "./components/home/Prodetail.vue";
 
-import login from "./components/home/login.vue";//登录界面
+import login from "./components/home/login.vue"; //登录界面
 import search from "./components/home/search.vue";
-import register from "./components/home/register.vue"
+import register from "./components/home/register.vue";
 
 import Vue from "vue";
 import VueRouter from "vue-router";
@@ -19,59 +19,58 @@ Vue.use(VueRouter);
 //重复点击报错问题解决
 const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch(err => err);
+    return originalPush.call(this, location).catch((err) => err);
 };
 
 export default new VueRouter({
-  routes: [
-   
-    {
-      path: "/",
-      redirect:"/favorite",
-      component: index,
-      children: [
+    routes: [
         {
-          path: "/favorite",
-          component: favorite
+            path: "/",
+            redirect: "/favorite",
+            component: index,
+            children: [
+                {
+                    path: "/favorite",
+                    component: favorite,
+                },
+                {
+                    path: "/classify",
+                    component: classify,
+                },
+                {
+                    path: "/roblist",
+                    component: roblist,
+                },
+                {
+                    path: "/shopcart",
+                    component: shopcart,
+                },
+                {
+                    path: "/mine",
+                    component: login,
+                },
+                {
+                    path: "/enter",
+                    component: mine,
+                },
+                {
+                    path: "/search",
+                    component: search,
+                },
+            ],
         },
         {
-          path: "/classify",
-          component: classify
+            path: "/rankinglist",
+            component: rankinglist,
         },
         {
-          path: "/roblist",
-          component: roblist
+            path: "/prodetail",
+            component: prodetail,
         },
         {
-          path: "/shopcart",
-          component: shopcart
+            path: "/register",
+            component: register,
         },
-        {
-          path: "/mine",
-          component: login
-        },
-        {
-          path:'/enter',
-          component: mine
-        },
-        {
-          path:"/search",
-          component: search
-        }
-      ]
-    },
-    {
-      path: "/rankinglist",
-      component: rankinglist
-    },
-    {
-      path:"/prodetail",
-      component:prodetail
-    },
-    {
-      path:"/register",
-      component:register
-    }
-  ]
+    ],
 });
 </script>
